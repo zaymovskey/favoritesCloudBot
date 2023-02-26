b5d7c0aa164cb4a8dbd71ba5674c7e837bd7e8f9
zaymovskey <zaymovskey@mail.com>
2023-02-26 18:52:15 +0300

    мелкий рефакторинг

--- loader.py
+++ loader.py
@@ -1,5 +1,4 @@
 from aiogram import Bot, Dispatcher
-
 from settings import config
 
 bot = Bot(config.bot_token.get_secret_value())
--- main.py
+++ main.py
@@ -1,6 +1,4 @@
 from aiogram import executor
-
-
 from routers import dp
 
 
